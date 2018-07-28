c394b6512b0d0e5141cf6051f0d45b03944274c9
Chris Russell <christine.russell10@gmail.com>
2018-07-28 09:55:30 -0400

    initial file create, template setup, firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    var config = {
+        apiKey: "AIzaSyBZSD7mFacLpbg91IZE_DvzhSXi7Q_lJvE",
+        authDomain: "trainscheduler-95d4f.firebaseapp.com",
+        databaseURL: "https://trainscheduler-95d4f.firebaseio.com",
+        projectId: "trainscheduler-95d4f",
+        storageBucket: "trainscheduler-95d4f.appspot.com",
+        messagingSenderId: "159299685363"
+    };
+    firebase.initializeApp(config);
+
+    //end of doc.ready
+});
