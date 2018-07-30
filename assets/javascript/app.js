07fcdb3d206524fd98cffbf0a60b00bd22c59dd0
Chris Russell <christine.russell10@gmail.com>
2018-07-30 18:13:25 -0400

    base HTML/CSS, add/retrieve train data

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,6 +8,64 @@ $(document).ready(function () {
         messagingSenderId: "159299685363"
     };
     firebase.initializeApp(config);
+    var database = firebase.database();
+    var $submit = $("#submit");
+    var $trainName = $("#trainName");
+    var $destination = $("#destination");
+    var $frequency = $("#frequency");
+    var $firstTrain = $("#firstTrain");
+    var $trainTable = $("#trainTable");
 
+    function addTrain(name, destination, firstTrain, frequency){
+        // console.log("Got here!");
+        console.log(name+" "+ destination+" "+ firstTrain+" "+ frequency);
+        database.ref().push({
+            name: name,
+            destination: destination,
+            firstTrain: firstTrain,
+            frequency: frequency,
+            dateAdded: firebase.database.ServerValue.TIMESTAMP
+          });
+    }
+
+    function calculateNextArrivalTime(firstTrain, frequency){
+
+    }
+
+    database.ref().on("child_added", function(childSnapshot) {
+      
+        // Log everything that's coming out of snapshot
+        var name = childSnapshot.val().name;
+        var destination = childSnapshot.val().destination;
+        var firstTrain = childSnapshot.val().firstTrain;
+        var frequency = childSnapshot.val().frequency;
+        
+        calculateNextArrivalTime(firstTrain, frequency);
+        
+        // full list of items to the well
+        var $newRow = $("<tr>");
+        var $nameCell = $("<td>" + name + "</td>");
+        var $destinationCell = $("<td>" + destination + "</td>");
+        var $frequencyCell = $("<td>" + frequency + "</td>");
+        var $nextArrivalCell = $("<td>");
+        var $minutesAwayCell = $("<td>");
+
+        // $nameCell.text(name);
+        $newRow.append($nameCell, $destinationCell, $frequencyCell, $nextArrivalCell, $minutesAwayCell);
+        $trainTable.append($newRow);
+              
+        // Handle the errors
+      }, function(errorObject) {
+        console.log("Errors handled: " + errorObject.code);
+      });
+
+    $submit.on("click", function(){
+        var destination = $destination.val();
+        var trainName = $trainName.val().trim();
+        var firstTrain = $firstTrain.val().trim();
+        var frequency = $frequency.val().trim();
+        addTrain(trainName, destination, firstTrain, frequency);
+        document.getElementById("trainInfo").reset();
+    });
     //end of doc.ready
 });
